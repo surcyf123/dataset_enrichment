73546aeb654e0b3dec31d5f006c6cf4dd9a226c0
root <surcyf@gmail.com>
2023-09-08 23:29:58 +0000

    dependecy script bug fix

--- enrichment_pipeline/reward_endpoint/install_dependencies.sh
+++ enrichment_pipeline/reward_endpoint/install_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 # curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && \
 # sudo apt-get install -y nodejs && \
 # sudo npm install -g npm && \
 # npm install -g pm2
+
 
 # Removing existing nodejs and npm
 sudo apt-get purge nodejs npm -y
@@ -12,13 +12,23 @@ sudo apt-get purge nodejs npm -y
 # Installing nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# Activating nvm
-source ~/.bashrc
+# Append nvm initialization to .bashrc
+echo -e "\n# NVM initialization" >> ~/.bashrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bashrc
+echo 'nvm use 14.21.3 > /dev/null 2>&1' >> ~/.bashrc
 
 # Installing specific node version
 nvm install 14.21.3
+nvm alias default 14.21.3
+
+# Source .bashrc to apply changes in this session
+source ~/.bashrc
 
 # Installing pm2 globally
 npm install -g pm2
 
 pip install flask
+pip install nvitop
+pip install torchmetrics
--- enrichment_pipeline/reward_endpoint/launch_reward_endpoints.py
+++ enrichment_pipeline/reward_endpoint/launch_reward_endpoints.py
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# if pm2 isn't installed
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && \
-# sudo apt-get install -y nodejs && \
-# sudo npm install -g npm && \
-# npm install -g pm2
-
-# if broken packages
-# sudo apt-get purge nodejs npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-# source ~/.bashrc
-# nvm install 14.21.3
-# npm install -g pm2
-
-
 START_PORT=30000
 NUM_INSTANCES=4 # Change to the number of GPUs you have / number of reward_instances you want to run.
-declare -i EXTERNAL_PORT=40357 #change this if you want to get a list of the endpoint urls
+declare -i EXTERNAL_PORT=40594 #change this if you want to get a list of the endpoint urls
 NAME=$START_PORT
 
 # Get the external IP address
@@ -33,7 +19,7 @@ do
     PORT=$((START_PORT + i))
 
     # Start the process with pm2
-    pm2 start --name "${PORT}" --time --interpreter=python3 /root/dataset_enrichment/enrichment_pipeline/reward_endpoint.py -- --gpu $GPU_ID --port $PORT
+    pm2 start --name "${PORT}" --time --interpreter=python3 /root/dataset_enrichment/enrichment_pipeline/reward_endpoint/reward_endpoint.py -- --gpu $GPU_ID --port $PORT
 
     # Append the URL for this reward endpoint to the urls array
     url_list+="\"http://$EXTERNAL_IP:$EXTERNAL_PORT\","
