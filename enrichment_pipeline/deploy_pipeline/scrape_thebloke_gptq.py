24db2d502d97401b6562aa9811988385433f78b3
Cole Miller <colemiller@MacBook-Air-2.local>
2023-09-09 21:53:41 -0400

    working scrape script

--- enrichment_pipeline/deploy_pipeline/scrape_thebloke_gptq.py
+++ enrichment_pipeline/deploy_pipeline/scrape_thebloke_gptq.py
@@ -1,25 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def get_gptq_models():
-    # Path to your webdriver (e.g., chromedriver)
-    driver_path = '/path/to/your/webdriver'
+    # Set up Chrome options
+    chrome_options = Options()
     
     # Initialize the browser
-    browser = webdriver.Chrome(driver_path)
+    browser = webdriver.Chrome(options=chrome_options)
     
     # URL of the HuggingFace profile
     url = "https://huggingface.co/TheBloke"
     browser.get(url)
     
-    # Wait for the models to load (you can adjust the time if needed)
-    browser.implicitly_wait(10)
+    # Use WebDriverWait along with expected_conditions to wait for a specific element to load
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
     
-    # Find all model names
-    model_elements = browser.find_elements(By.CSS_SELECTOR, 'a.hf-Link')  # Modify the selector if it's different
+    # Find all links
+    model_elements = browser.find_elements(By.TAG_NAME, 'a')
     
-    # Filter model names containing "GPTQ" or "gptq"
-    gptq_models = [elem.text for elem in model_elements if "GPTQ" in elem.text or "gptq" in elem.text]
+    # Filter model names containing "GPTQ" or "gptq" and clean up the names
+    gptq_models = [elem.text.split('\n')[0] for elem in model_elements if "GPTQ" in elem.text or "gptq" in elem.text]
     
     # Close the browser
     browser.quit()
