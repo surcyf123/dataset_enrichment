ee9178816fa75b285f100b505c023c07271d6faf
surcyf123 <surcyf@gmail.com>
2023-09-15 00:13:19 -0400

    now using exllama

--- enrichment_pipeline/prod/host_gptq.py
+++ enrichment_pipeline/prod/host_gptq.py
@@ -4,63 +4,65 @@ model = sys.argv[1]
 port = int(sys.argv[2])
 gpu_id = int(sys.argv[3])
 gpu_type = sys.argv[4]
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from flask import Flask, request, jsonify
+from exllamav2 import(
+    ExLlamaV2,
+    ExLlamaV2Config,
+    ExLlamaV2Cache,
+    ExLlamaV2Tokenizer,
+)
 
-import accelerate
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria, StoppingCriteriaList
-from typing import List
+from exllamav2.generator import (
+    ExLlamaV2BaseGenerator,
+    ExLlamaV2Sampler
+)
 
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path,
-                                          use_fast=False,
-                                          local_files_only=True,
-                                          trust_remote_code=True)
-class StoppingCriteriaSub(StoppingCriteria):
-    def __init__(self, stops = []):
-        super().__init__()
-        self.stops = [stop.to("cuda:{gpuid}") for stop in stops]
+config = ExLlamaV2Config()
+config.model_dir = model_directory
+config.prepare()
 
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
-        for stop in self.stops:
-            if torch.all((stop == input_ids[0][-len(stop):])).item():
-                return True
-        return False
+model = ExLlamaV2(config)
+print("Loading model: " + model_directory)
+model.load([18, 24])
 
+tokenizer = ExLlamaV2Tokenizer(config)
 
-def convert_stopwords_to_ids(stopwords : List[str]):
-    stop_words_ids = [tokenizer(stop_word, return_tensors='pt')['input_ids'].squeeze() for stop_word in stopwords]
-    stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
-    return stopping_criteria
+cache = ExLlamaV2Cache(model)
 
+# Initialize generator
 
+generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
                                             local_files_only=True,
                                            torch_dtype=torch.float16,
                                            trust_remote_code=True,
                                            device_map=f"cuda:{gpuid}")
 
-def generate_output(text: str, num_responses: int, max_new_tokens, temperature, top_p, top_k, repetition_penalty, stop_tokens):
-    # Convert the text to input_ids
-    input_ids = tokenizer(text, return_tensors="pt").input_ids.to(f"cuda:{gpuid}")
+def generate_output(text: str, max_new_tokens, temperature, top_p, top_k, repetition_penalty,stopwords):
+
+    settings = ExLlamaV2Sampler.Settings()
+    settings.temperature = temperature
+    settings.top_k = top_k
+    settings.top_p = top_p
+    settings.token_repetition_penalty = repetition_penalty
+    settings.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
     
-    # Use num_return_sequences to generate multiple completions in parallel
-    tokens = model.generate(
-        inputs=input_ids,
-        max_new_tokens=max_new_tokens,
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        repetition_penalty=repetition_penalty,
-        stopping_criteria=convert_stopwords_to_ids(stop_tokens),
-        do_sample=True,
-        num_return_sequences=num_responses
-    )
+    generator.warmup()
+    time_begin = time.time()
+
+    output = generator.generate_simple(text, settings, max_new_tokens, seed = 1234)
+
+    time_end = time.time()
+    time_total = time_end - time_begin
+    
+    print(f"Response generated in {time_total:.2f} seconds, {max_new_tokens} tokens, {max_new_tokens / time_total:.2f} tokens/second")
+    t_per_s = (max_new_tokens / time_total)
     
     # Decode each item in the batch
-    return [tokenizer.decode(token, skip_special_tokens=True) for token in tokens]
-
-
+    return output,t_per_s
 
 app = Flask(__name__)
 import re
@@ -68,36 +70,27 @@ import re
 @app.route('/generate', methods=['POST'])
 def generate_text():
     if gpu_type == "3090":
-        num_tokens = 200
+        num_tokens = 115
     elif gpu_type == "4090":
-        num_tokens = 250
+        num_tokens = 150
     else:
         raise ValueError(f"Invalid gpu_type: {gpu_type}")
     data = request.json
-    num_responses = 4  # Number of varied responses for each prompt
+    num_responses = 3  # Number of varied responses for each prompt
     
     # Generate multiple outputs for the prompt
-    responses = generate_output(
+    responses,t_per_s = generate_output(
         data['prompt'],
-        num_responses,
         num_tokens,
-        0.95,
+        0.9,
         1.0,
-        90,
+        80,
         1.0,
-        []
-    )
+        [])
     
-    # Clean up the responses
-    for i, response in enumerate(responses):
-        pruned_prompt = re.sub('<\|.*?\|>', '', data['prompt'])
-        responses[i] = response.replace(pruned_prompt, "")
-        
-        # Remove StopToken from the Generation
-        for stop in data.get('stopwords', []):
-            responses[i] = responses[i].replace(stop, "")
     
-    return jsonify({'response': responses, "model": model_name_or_path})
+    
+    return jsonify({'response': responses, "model": model_directory, "tokens_per_second" : t_per_s})
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=local_port)
