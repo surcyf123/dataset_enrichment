7b9ba5d701362da31f38ad61ef75031c99d4f5aa
surcyf123 <surcyf@gmail.com>
2023-09-20 12:18:41 +0100

    new version

--- enrichment_pipeline/prod/host_gptq.py
+++ enrichment_pipeline/prod/host_gptq.py
@@ -26,31 +26,30 @@ from exllamav2.generator import (ExLlamaV2BaseGenerator, ExLlamaV2Sampler)
 config = ExLlamaV2Config()
 config.model_dir = model_directory
 config.prepare()
-
 model = ExLlamaV2(config)
-print("Loading model: " + model_directory)
+print("Loading model:", model_directory)
 model.load([18, 24])
 tokenizer = ExLlamaV2Tokenizer(config)
 
-def generate_output(text: str, max_new_tokens, temperature, top_p, top_k, repetition_penalty, stopwords, num_completions):
+# Initialize cache and generator
+cache = ExLlamaV2Cache(model)
+generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
 
-    # Initialize the cache and generator inside the function
-    cache = ExLlamaV2Cache(model, batch_size=num_completions)
-    generator = ExLlamaV2BaseGenerator(model, cache, tokenizer)
+# Warm up the model
+generator.warmup()
 
+def generate_output(text, max_new_tokens, temperature, top_p, top_k, repetition_penalty, stopwords, num_completions):
+    """Generate text completions based on the given settings."""
     settings = ExLlamaV2Sampler.Settings()
     settings.temperature = temperature
     settings.top_k = top_k
     settings.top_p = top_p
     settings.token_repetition_penalty = repetition_penalty
     settings.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
-    settings.token_bias = None
-    
-    # Create a list of prompts
+
     prompts = [text] * num_completions
-    time.begin = time.time()
-    generator.warmup()
-    full_texts = generator.generate_simple(prompts, settings, max_new_tokens, seed=None)
+    time_begin = time.time()
+    full_texts = generator.generate_simple(prompts, settings, max_new_tokens)
     outputs = [full_text[len(text):] for full_text in full_texts]
 
     time_taken = time.time() - time_begin
